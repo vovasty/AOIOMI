d5e11a3bad6ee50a52b64d625e1d4322b6fac7a7
Vlad Solomenchuk <vlsolome@coupang.com>
2021-09-02 19:56:38 -0700

    fix replaceresponsecontent.py: open json as binary

--- MITMProxyAddons/Sources/MITMProxyAddons/Resources/python/replaceresponsecontentaddon.py
+++ MITMProxyAddons/Sources/MITMProxyAddons/Resources/python/replaceresponsecontentaddon.py
@@ -6,12 +6,12 @@ import re
 
 class ReplaceResponseContentAddon:
     def __init__(self, config):
-        with open(config, 'r') as file:
+        with open(config, 'rb') as file:
             data = json.load(file)
             config = []
-            for key, value in data.items():
-                config.append((re.compile(key), bytes(value, "utf8")))
-            self.config = config
+        for key, value in data.items():
+            config.append((re.compile(key), bytes(value, "utf8")))
+        self.config = config
             
 
     def response(self, flow: http.HTTPFlow):
